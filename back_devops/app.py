fed15df73f79f09b1a0406d51b8538c9dd5a3c12
Daniel Perez <dra.perez@yavirac.edu.ec>
2026-07-09 00:09:50 -0500

    Corregir import del backend para Docker

--- back_devops/app.py
+++ back_devops/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from back_devops.database import get_connection
+from database import get_connection
 
 app = Flask(__name__)
 CORS(app)
